b3945e525f38f9ad5480d22c3d0dded536220030
Romulo Domiciano <domiciano_romulo@hotmail.com>
2022-01-28 18:02:09 -0300

    Added texts to dictionary

--- lib/dictionary/eng_us.dart
+++ lib/dictionary/eng_us.dart
@@ -1,1 +1,10 @@
-Map<String, String> en_us = {};
+// ignore: non_constant_identifier_names
+Map<String, String> en_us = {
+  //Login screen
+  'create_account': 'Criar Conta',
+  'does_not_have_account': 'Não tem conta ainda? Clique no botão \"CRIAR CADASTRO"',
+  'email': 'E-mail',
+  'enter': 'Entrar',
+  'forgotPassword': 'Esquecia a senha',
+  'password': 'Senha',
+};
